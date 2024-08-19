8d0dd8a4a8973b47381993fa2cb391c75da18e70
brand001 <b9950463164@gmail.com>
2024-08-19 15:20:50 +0800

    app nevis

--- app/src/setting/Pin.js
+++ app/src/setting/Pin.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { View, Text, Dimensions } from "react-native";
+import { View, Text } from "react-native";
 import styles from '$NevisStyles/Pin'
 import translate from '$Nevis/translate'
+import Verify from '../verify/index'
+import Touch from 'react-native-touch-once';
 
 
 class Pin extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            onVerify: false
         }
     }
 
@@ -20,12 +22,36 @@ class Pin extends React.Component {
 		
     }
 
+    onVerify = () => {
+        this.setState({onVerify: true})
+    }
+
+    onSuccess = () => {
+        this.setState({onVerify: false})
+    }
+
+    onError = () => {
+        this.setState({onVerify: false})
+    }
 
     render() {
 		
+        const { onVerify } = this.state
+
         return (
             <View>
 				<Text>Pin</Text>
+                <Touch onPress={() => { this.onVerify() }}>
+                    <Text>添加Pin验证</Text>
+                </Touch>
+                {
+                    onVerify &&
+                    <Verify
+                        modeType={'Pin'}
+                        onSuccess={() => { this.onSuccess() }}//验证/添加成功
+                        onError={() => { this.onError() }}//验证/添加失败
+                />
+                }
             </View>
         )
     }
