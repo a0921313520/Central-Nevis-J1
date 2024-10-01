edebaa815c9967429e3427a4c9bf457aaa8788cd
judy012378 <judy2769@gmail.com>
2024-10-01 20:51:15 +0800

    版本号

--- app/src/index.js
+++ app/src/index.js
@@ -42,7 +42,7 @@ class Nevis extends React.Component {
         }
         this.config = getConfig()
         window.NToast = this.config.NevisToast
-        window.JBOVersion = '1.0.2.6'
+        window.JBOVersion = '1.0.2.7'
     }
 
     componentDidMount() {
